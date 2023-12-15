37bcf4a943f5264ba676094d047ac4df66a015ac
Ibraheem Rodrigues <28712361+IbraheemR@users.noreply.github.com>
2023-12-15 23:35:11 +0000

    Make days until comp counter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    const T = Date.parse('01 Jul 2024 00:00:00 GMT').valueOf();
+
+    let now = Date.now();
+
+    setInterval(() => {
+        now = Date.now();
+    }, 100)
+
+    let diff, secs, mins, hours, days;
+
+    $: {
+        diff = T - now;
+        secs = Math.floor(diff/1000);
+        mins = Math.floor(diff/1000/60);
+        hours= Math.floor(diff/1000/60/60);
+        days = Math.floor(diff/1000/60/60/24);
+    }
+
+</script>
+<div class="text-[20vmin] h-screen flex items-center justify-center flex-col">
+    <div class="m-10">
+        {days} days 
+    </div>
+    <div class="text-[10vmin] m-10">until comp</div>
+</div>
