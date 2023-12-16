6f7f9d32761f518cdcef126d2fe8c7f5d3ee843b
Ibraheem Rodrigues <28712361+IbraheemR@users.noreply.github.com>
2023-12-16 19:23:37 +0000

    Add Dhms display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,17 +13,27 @@
 
     $: {
         diff = T - now;
-        secs = Math.floor(diff/1000);
-        mins = Math.floor(diff/1000/60);
-        hours= Math.floor(diff/1000/60/60);
         days = Math.floor(diff/1000/60/60/24);
+        diff = diff - days*1000*60*60*24;
+        hours= Math.floor(diff/1000/60/60);        
+        diff = diff - hours*1000*60;
+        mins = Math.floor(diff/1000/60);
+        diff = diff - mins*1000*60;
+        secs = Math.floor(diff/1000);
     }
 
 </script>
 
 <div class="text-[20vmin] h-screen flex items-center justify-center flex-col">
     <div>
-        {days} days 
+        {days}
+        <span class="text-[10vmin]">D</span>
+        {hours}
+        <span class="text-[10vmin]">h</span>
+        {mins}
+        <span class="text-[10vmin]">m</span>
+        {secs}
+        <span class="text-[10vmin]">s</span>
     </div>
     <div class="text-[10vmin]">until comp</div>
     <div class="text-[5vmin] mt-[20vh]">bad things Joao said: 
